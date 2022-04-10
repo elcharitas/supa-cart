d5f2b6fee7efc289f9d0e2b2a71466c01083f0cc
elcharitas <jonathanirhodia@gmail.com>
2022-04-10 13:03:48 +0100

    chore: change logo path

--- components/Logo.js
+++ components/Logo.js
@@ -3,7 +3,7 @@ import { Image, Text } from "@chakra-ui/react";
 export default function Logo() {
     return (
         <>
-            <Image src="./logo.png" w="8" />
+            <Image src="/logo.png" w="8" />
             <Text fontWeight="extrabold" textDecor="none">
                 SupaCart
             </Text>
