e26b3289f3b636c393159cbbca6c65712c804241
elcharitas <jonathanirhodia@gmail.com>
2022-04-10 01:36:37 +0100

    chore: add providers to base component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import Link from "next/link";
+import { SaasProvider, AuthProvider, ModalsProvider } from "@saas-ui/react";
+
+import { theme } from "../utils/theme";
+import { supabaseService } from "../utils/supabase";
+
+// Saas UI components will use the Next Link Wrapper.
+const NextLink = (props) => <Link passHref {...props} />;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <SaasProvider theme={theme} linkComponent={NextLink}>
+            <AuthProvider {...supabaseService}>
+                <ModalsProvider>
+                    <Component {...pageProps} />
+                </ModalsProvider>
+            </AuthProvider>
+        </SaasProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
